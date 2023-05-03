2fb482a605b8d4cc3a721dc95b029293286e0026
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-05-03 11:38:17 +0530

    create login xml

--- app/src/main/java/com/example/billtrackermobile/RegisterActivity.java
+++ app/src/main/java/com/example/billtrackermobile/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.billtrackermobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        findViewById(R.id.signUpDirect).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
     }
 }
