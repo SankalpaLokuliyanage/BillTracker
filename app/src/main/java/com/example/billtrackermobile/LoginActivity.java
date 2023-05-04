bef4d7d145a59bd02b4435faac8e5b01743cffe5
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-05-04 12:38:24 +0530

    made login

--- app/src/main/java/com/example/billtrackermobile/LoginActivity.java
+++ app/src/main/java/com/example/billtrackermobile/LoginActivity.java
@@ -1,22 +1,77 @@
 package com.example.billtrackermobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+        Button Signin;
+        EditText email, password;
+
+        FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        auth = FirebaseAuth.getInstance();
+
+        Signin = findViewById(R.id.signinText);
+        email = findViewById(R.id.signin_email);
+        password = findViewById(R.id.signin_password);
+
         findViewById(R.id.signInDirect).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginUser();
+            }
+        });
+    }
+    private void LoginUser() {
+        String userEmail = email.getText().toString();
+        String userPass = password.getText().toString();
+
+
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this, "Please input your email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPass)) {
+            Toast.makeText(this, "Please make a password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(userEmail, userPass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
