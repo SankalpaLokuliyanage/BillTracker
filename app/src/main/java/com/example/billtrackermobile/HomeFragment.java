0f21c6af4c6b4f63dabcc2cd8d7e77038b69f3f0
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-05-04 15:56:14 +0530

    make changes

--- app/src/main/java/com/example/billtrackermobile/HomeFragment.java
+++ app/src/main/java/com/example/billtrackermobile/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.billtrackermobile;
 
+
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,13 +12,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+
+import java.util.ArrayList;
+
+
 public class HomeFragment extends Fragment {
+
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        return root;
     }
 }
