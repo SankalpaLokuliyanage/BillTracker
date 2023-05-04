4d1c81af66855c2c69ca99867a049d9768c2843a
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-05-04 14:47:34 +0530

    get name from database

--- app/src/main/java/com/example/billtrackermobile/MainActivity.java
+++ app/src/main/java/com/example/billtrackermobile/MainActivity.java
@@ -9,25 +9,58 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.example.billtrackermobile.models.userModels;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     private DrawerLayout drawerLayout;
+
+    private com.example.billtrackermobile.databinding.ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
+        binding = com.example.billtrackermobile.databinding.ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavigationView navigationView1 = binding.navView;
+        View headerView = navigationView1.getHeaderView(0);
+        TextView headerName = headerView.findViewById(R.id.nav_header_name);
+
+        database.getReference().child("users").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        userModels userModel = snapshot.getValue(userModels.class);
+
+                        headerName.setText(userModel.getName());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
@@ -85,6 +118,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
     }
+
+
 
 
 }
--- app/src/main/java/com/example/billtrackermobile/ProfileFragment.java
+++ app/src/main/java/com/example/billtrackermobile/ProfileFragment.java
@@ -2,20 +2,61 @@ package com.example.billtrackermobile;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.billtrackermobile.models.userModels;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ProfileFragment extends Fragment {
 
+    TextView name;
+
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        name = root.findViewById(R.id.username);
+
+        database.getReference().child("users").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        userModels userModel = snapshot.getValue(userModels.class);
+                        name.setText(userModel.getName());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+
+
+
+        return root;
+
+
     }
+
 }
