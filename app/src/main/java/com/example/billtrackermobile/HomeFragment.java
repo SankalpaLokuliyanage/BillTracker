41a975f0010e8f85dd30cd2eae2ebe6cd0e22ed4
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-05-06 11:29:06 +0530

    start to develop home fragment

--- app/src/main/java/com/example/billtrackermobile/HomeFragment.java
+++ app/src/main/java/com/example/billtrackermobile/HomeFragment.java
@@ -10,15 +10,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
+import com.example.billtrackermobile.models.userModels;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 
 public class HomeFragment extends Fragment {
 
+    TextView number;
 
+    FirebaseAuth auth;
+    FirebaseDatabase database;
 
 
     @Override
@@ -26,6 +36,25 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        number = root.findViewById(R.id.usernumber);
+
+        database.getReference().child("users").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        userModels userModel = snapshot.getValue(userModels.class);
+                        number.setText(userModel.getNumber());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
 
 
         return root;
