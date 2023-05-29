48ace7cd57c9f2a075ac9184bf475d6ace4ae431
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-05-29 12:57:48 +0530

    successfully complete promo / offers images on recyclerview

--- app/src/main/java/com/example/billtrackermobile/HomeFragment.java
+++ app/src/main/java/com/example/billtrackermobile/HomeFragment.java
@@ -6,21 +6,30 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-import com.example.billtrackermobile.models.TitleCategories;
+import com.example.billtrackermobile.Adaptors.PromoAdaptor;
+import com.example.billtrackermobile.models.PromoModel;
+//import com.example.billtrackermobile.models.TitleCategories;
 import com.example.billtrackermobile.models.userModels;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,6 +43,14 @@ public class HomeFragment extends Fragment {
     FirebaseAuth auth;
     FirebaseDatabase database;
 
+    FirebaseFirestore db;
+
+    RecyclerView PromoRec;
+
+    List<PromoModel> promoModelList;
+    PromoAdaptor promoAdaptor;
+
+
 
 
     @Override
@@ -44,6 +61,7 @@ public class HomeFragment extends Fragment {
 
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         number = root.findViewById(R.id.usernumber);
 
@@ -58,6 +76,30 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
 
+                    }
+                });
+
+
+        PromoRec = root.findViewById(R.id.promorec);
+
+        PromoRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        promoModelList = new ArrayList<>();
+        promoAdaptor = new PromoAdaptor(getActivity(), promoModelList);
+        PromoRec.setAdapter(promoAdaptor);
+
+        db.collection("PromoImages").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                PromoModel promoModel = documentSnapshot.toObject(PromoModel.class);
+                                promoModelList.add(promoModel);
+                                promoAdaptor.notifyDataSetChanged();
+                            }
+                        } else {
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
 
