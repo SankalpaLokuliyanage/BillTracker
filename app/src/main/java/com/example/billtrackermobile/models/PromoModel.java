422609571e531872027d854f6778fcb33665975e
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2023-05-29 11:52:41 +0530

    promomodel

--- app/src/main/java/com/example/billtrackermobile/models/PromoModel.java
+++ app/src/main/java/com/example/billtrackermobile/models/PromoModel.java
@@ -1,5 +1,29 @@
 package com.example.billtrackermobile.models;
 
 public class PromoModel {
-    
+    String type;
+    String img_url;
+
+    public PromoModel(){}
+
+    public PromoModel(String type, String img_url) {
+        this.type = type;
+        this.img_url = img_url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
 }
